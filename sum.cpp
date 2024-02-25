6f2087ab827676543bd11421ed2e27be8c1b53b4
Anurag Saran <160638946+AnuragSaran@users.noreply.github.com>
2024-02-25 23:16:13 +0530

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"enter no.:";
+    cin>>a;
+    cout<<"enter no.:";
+    cin>>b;
+    c=a+b;
+    cout<<"sum of a+b="<<c;
+    return 0;
+
+
+
+}
---
+++ swapping.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"enter no.:";
+    cin>>a;
+    cout<<"enter no.:";
+    cin>>b;
+    c=a;
+    a=b;
+    b=c;
+     cout<<a<<endl;
+
+    cout<< b;
+
+    return 0;
+
+}
